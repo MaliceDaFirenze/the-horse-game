b1ae0f82b0f027fa0898198d37917f265e978de5
Alice Ruppert <alice.ruppert@gmx.ch>
2018-10-26 07:56:51 +0200

    - improved obstacle model parenting & rearranged - added obstacles in storage - obstacles in storage functionality WIP

---
+++ Assets/Scripts/Interactables/ObstacleStorage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleStorage : Interactable {
+
+	public override void PlayerInteracts(Player player){
+		base.PlayerInteracts (player);
+		switch (currentlyRelevantActionIDs [selectedInteractionIndex]) {
+		case actionID.REARRANGE_OBSTACLES:
+			//TODO 
+			break;
+		}
+	}
+
+
+	public override List<string> DefineInteraction (Player player)	{
+
+		List<string> result = new List<string> ();
+		currentlyRelevantActionIDs.Clear();
+
+		switch (player.currentlyEquippedItem.id) {
+		case equippableItemID.BAREHANDS:
+			if (player.currentlyEquippedItem.status == containerStatus.FULL) {
+				currentlyRelevantActionIDs.Add (actionID.REARRANGE_OBSTACLES);
+				result.Add(InteractionStrings.GetInteractionStringById(actionID.REARRANGE_OBSTACLES));
+			} 
+			break;
+		}
+
+		return result;
+	}
+}
