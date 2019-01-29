1d4f5d0e897c3c25656aba81adaab3bc96ba95e2
Alice Ruppert <alice.ruppert@gmx.ch>
2019-01-29 08:04:06 +0100

    - added hay/straw bale model - arranged hay and straw in store - added StoreCase script - edited building camera pos - arranged apple store case

---
+++ Assets/Scripts/Interactables/StoreCase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreCase : Interactable {
+
+	Horse_Interactable horseTiedHere;
+
+	public override void PlayerInteracts(Player player){
+		base.PlayerInteracts (player);
+
+		switch (currentlyRelevantActionIDs [selectedInteractionIndex]) {
+		case actionID.OPEN_SHOP:
+			break;
+		}
+	}
+
+	public override List<string> DefineInteraction (Player player)	{
+		List<string> result = new List<string> ();
+		currentlyRelevantActionIDs.Clear();
+
+		switch (player.currentlyEquippedItem.id) {
+		case equippableItemID.BAREHANDS:
+			currentlyRelevantActionIDs.Add(actionID.OPEN_SHOP);
+			result.Add(InteractionStrings.GetInteractionStringById(actionID.OPEN_SHOP));
+			break;
+		}
+
+		return result;
+
+	}
+}
