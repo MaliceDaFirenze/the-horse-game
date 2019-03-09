456b10b973141d02050e76f5b9ae1404a234ce34
Alice Ruppert <alice.ruppert@gmx.ch>
2019-03-09 09:16:09 +0100

    created stall unit script for walls reference

---
+++ Assets/StallPaddockUnit.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StallPaddockUnit : MonoBehaviour {
+
+	public GameObject rightWall;
+	public GameObject leftWall;
+}
