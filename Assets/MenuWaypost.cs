5dd0b6c8124d98f8b4f28a3cb329d29f4e90a808
Alice Ruppert <alice.ruppert@gmx.ch>
2019-04-11 08:06:00 +0200

    replaced horse model with hooves, menu functionality WIP

---
+++ Assets/MenuWaypost.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuWaypost : MonoBehaviour {
+
+	public void ClickSign(int index){
+		
+	}
+}
---
+++ Assets/MenuWaypostSign.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MenuWaypostSign : MonoBehaviour {
+	MenuWaypost menu;
+	public int signIndex;
+
+	private void OnMouseDown (){
+		menu.ClickSign (signIndex);
+
+		//0: load main scene with forcenewgame disabled
+		//1: load main scene with forcenewgame enabled
+		//2: load credits scene
+		//3: quit application
+	}
+}
