eb23eac9823d312d27764f21d558cd2953aaf7af
Alice Ruppert <alice.ruppert@gmx.ch>
2019-04-15 08:04:44 +0200

    menu waypost: scale and color change on interaction

--- Assets/MenuWaypostSign.cs
+++ Assets/MenuWaypostSign.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 public class MenuWaypostSign : MonoBehaviour {
 	MenuWaypost menu;
+	private MeshRenderer meshRenderer;
 	public int signIndex;
 
+	private Vector3 regularScale;
+	private Vector3 activeScale;
+
+	public Material[] regularMats;
+	public Material[] activeMats;
+
 	private void Start(){
+		regularScale = transform.localScale;
+		activeScale = regularScale * 1.1f;
 		menu = transform.parent.GetComponent<MenuWaypost> ();
+		meshRenderer = GetComponent<MeshRenderer> ();
 	}
 
 	private void OnMouseDown (){
 		menu.ClickSign (signIndex);
-	
+		meshRenderer.materials = activeMats;
+		StartCoroutine (ReturnToRegular ());
+	}
+
+	private IEnumerator ReturnToRegular(){
+		yield return new WaitForSeconds (0.5f);
+		meshRenderer.materials = regularMats;
+
+	}
+
+	private void OnMouseEnter (){
+		LeanTween.scale (gameObject, activeScale, 0.3f);
+	}
+
+	private void OnMouseExit (){
+		LeanTween.scale (gameObject, regularScale, 0.3f);
 	}
 }
