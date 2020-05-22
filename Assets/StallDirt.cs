7d3d676d13068b1999bd36d6fb02b81c819a8c4d
Alice Ruppert <alice.ruppert@gmx.ch>
2020-05-22 16:36:45 +0200

    added stall dirt gradient, increase level every day

---
+++ Assets/StallDirt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StallDirt : TimeDependentObject {
+
+	public Gradient colorProgression;
+	public MeshRenderer[] strawParts;
+	private float dirtLevel = 0;
+	private float dirtLevelIncreasePerDay = 0.15f;
+
+	public override void StartNewDay(){
+		dirtLevel += dirtLevelIncreasePerDay;
+
+		if (dirtLevel > 1) {
+			dirtLevel = 1;
+		}
+
+		foreach (MeshRenderer mr in strawParts) {
+			mr.materials [0].color = colorProgression.Evaluate (dirtLevel);
+		}
+	}
+}
